4f9d108693af6b723dee5088c5f98d25e8e4a06a
Kevin On <kevinon@iris-ws-5.stanford.edu>
2026-09-23 09:12:38 -0700

    Record workstation network and ZED camera defaults

--- droid/misc/parameters.py
+++ droid/misc/parameters.py
@@ -2,17 +2,17 @@ import os
 from cv2 import aruco
 
 # Robot Params #
-nuc_ip = '172.16.0.1'
-robot_ip = '172.16.0.2'
-laptop_ip = ""
+nuc_ip = "172.16.0.1"
+robot_ip = "172.16.0.2"
+laptop_ip = "172.16.0.10"
 sudo_password = ""
-robot_type = "panda"  # 'panda' or 'fr3'
+robot_type = "panda"
 robot_serial_number = "295341-1325480"
 
 # Camera ID's #
-hand_camera_id = ""
-varied_camera_1_id = ""
-varied_camera_2_id = ""
+hand_camera_id = "15577469"
+varied_camera_1_id = "38651013"
+varied_camera_2_id = "29838012"
 
 # Charuco Board Params #
 CHARUCOBOARD_ROWCOUNT = 9
